0882529a3b63060994b50dc70fa776f1f438ea4c
Sifan YE <ysf199711@126.com>
2018-06-27 15:11:25 +0200

    Initial Commit

---
+++ AppleWatchHeartRateCollectionUITests/AppleWatchHeartRateCollectionUITests.swift
@@ -0,0 +1,36 @@
+//
+//  AppleWatchHeartRateCollectionUITests.swift
+//  AppleWatchHeartRateCollectionUITests
+//
+//  Created by 叶思帆 on 27/06/2018.
+//  Copyright © 2018 Sifan Ye. All rights reserved.
+//
+
+import XCTest
+
+class AppleWatchHeartRateCollectionUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
