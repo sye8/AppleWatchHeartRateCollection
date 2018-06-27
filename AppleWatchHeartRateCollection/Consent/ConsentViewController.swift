9aa99e77c61af2c77d1705f07d702aae9957dae0
Sifan YE <ysf199711@126.com>
2018-06-27 15:21:30 +0200

    Init

---
+++ AppleWatchHeartRateCollection/Consent/ConsentViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ConsentViewController.swift
+//  AppleWatchHeartRateCollection
+//
+//  Created by 叶思帆 on 27/06/2018.
+//  Copyright © 2018 Sifan Ye. All rights reserved.
+//
+
+import ResearchKit
+
+class ConsentViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
--- AppleWatchHeartRateCollection/IntroViewController.swift
+++ AppleWatchHeartRateCollection/IntroViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class IntroViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
