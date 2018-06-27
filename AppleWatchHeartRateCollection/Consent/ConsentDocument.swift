e3a9afefe7b4e182f6e4dd15ae73f5af3eb6e80c
Sifan YE <ysf199711@126.com>
2018-06-27 17:11:42 +0200

    Section details

---
+++ AppleWatchHeartRateCollection/Consent/ConsentDocument.swift
@@ -0,0 +1,64 @@
+//
+//  ConsentDocument.swift
+//  ResearchKitTutorial
+//
+//  Created by 叶思帆 on 27/06/2018.
+//  Copyright © 2018 Sifan Ye. All rights reserved.
+//
+
+import ResearchKit
+
+public var ConsentDocument: ORKConsentDocument {
+    
+    let consentDocument = ORKConsentDocument()
+    consentDocument.title = "Example Consent"
+    
+    //TODO: consent sections
+    let sectionTypes: [ORKConsentSectionType] = [
+        .overview,
+        .dataGathering,
+        .privacy,
+        .dataUse,
+        .timeCommitment,
+        .studySurvey,
+        .studyTasks,
+        .withdrawing
+    ]
+    
+    let text = [
+        "Section 1: Welcome. This study is about...",
+        "Section 2: Data Gathering. This study will collect data from your Apple Watch...",
+        "Section 3: Privacy. We value your privacy...",
+        "Section 4: Data Use. The data collected will be used for...",
+        "Section 5: Time Commitment. This study will take you roughly...",
+        "Section 6: Study Survey. For this study, you will need to fill out a survey...",
+        "Section 7: Study Tasks. You will be requested to do these tasks...",
+        "Section 8: Withdrawing. To withdraw from the study..."
+    ]
+    
+    consentDocument.sections = []
+    
+    for sectionType in sectionTypes {
+        let section = ORKConsentSection(type: sectionType)
+        
+        let localizedText = NSLocalizedString(text[sectionTypes.index(of: sectionType)!], comment: "")
+        let localizedSummary = localizedText.components(separatedBy: ".")[0] + "."
+        
+        section.summary = localizedSummary
+        section.content = localizedText
+        
+        if consentDocument.sections == nil {
+            consentDocument.sections = [section]
+        } else {
+            consentDocument.sections!.append(section)
+        }
+    }
+    
+    
+    
+    //TODO: signature
+    consentDocument.addSignature(ORKConsentSignature(forPersonWithTitle: nil, dateFormatString: nil, identifier: "ConsentDocumentParticipantSignature"))
+    
+    
+    return consentDocument
+}
---
+++ AppleWatchHeartRateCollection/Consent/ConsentTask.swift
@@ -0,0 +1,31 @@
+//
+//  ConsentTask.swift
+//  ResearchKitTutorial
+//
+//  Created by 叶思帆 on 27/06/2018.
+//  Copyright © 2018 Sifan Ye. All rights reserved.
+//
+
+import ResearchKit
+
+public var ConsentTask: ORKOrderedTask {
+    
+    var steps = [ORKStep]()
+    
+    //TODO: Add VisualConsentStep
+    let consentDocument = ConsentDocument
+    let visualConsentStep = ORKVisualConsentStep(identifier: "VisualConsentStep", document: consentDocument)
+    steps += [visualConsentStep]
+    
+    //TODO: Add ConsentReviewStep
+    let signature = consentDocument.signatures!.first as! ORKConsentSignature
+    
+    let reviewConsentStep = ORKConsentReviewStep(identifier: "ConsentReviewStep", signature: signature, in: consentDocument)
+    
+    reviewConsentStep.text = "Review Consent!"
+    reviewConsentStep.reasonForConsent = "Consent to join study"
+    
+    steps += [reviewConsentStep]
+    
+    return ORKOrderedTask(identifier: "ConsentTask", steps: steps)
+}
