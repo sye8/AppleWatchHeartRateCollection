a5c07c457c9d00215b6164a075e3b5e6226353e7
Sifan YE <ysf199711@126.com>
2018-06-28 17:02:35 +0200

    Survey (Runtime error)

---
+++ AppleWatchHeartRateCollection/Tasks/SurveyTask.swift
@@ -0,0 +1,66 @@
+//
+//  SurveyTask.swift
+//  AppleWatchHeartRateCollection
+//
+//  Created by 叶思帆 on 28/06/2018.
+//  Copyright © 2018 Sifan Ye. All rights reserved.
+//
+
+import ResearchKit
+
+public var SurveyTask: ORKOrderedTask {
+    
+    var steps = [ORKStep]()
+    
+    //Instruction
+    let instructionStep = ORKInstructionStep(identifier: "IntroStep")
+    instructionStep.title = "Survey Instructions"
+    instructionStep.text = "This survey will provide us..."
+    steps += [instructionStep]
+    
+    //Short text Input Question
+    let shortAnswerFormat = ORKTextAnswerFormat(maximumLength: 32)
+    shortAnswerFormat.multipleLines = false
+    let shortAnswerStepTitle = "Single Line, Short Answer, 36 chars"
+    let shortAnswerStep = ORKQuestionStep(identifier: "QuestionStep", title: shortAnswerStepTitle, answer: shortAnswerFormat)
+    steps += [shortAnswerStep]
+    
+    //Text Choice Question
+    let textChoiceStepTitle = "Text Choice"
+    let textChoices = [
+        ORKTextChoice(text: "Choice 1", value: 0 as NSCoding & NSCopying & NSObjectProtocol),
+        ORKTextChoice(text: "Choice 2", value: 1 as NSCoding & NSCopying & NSObjectProtocol),
+        ORKTextChoice(text: "Choice 3", value: 2 as NSCoding & NSCopying & NSObjectProtocol)
+    ]
+    let textChoiceAnswerFormat: ORKTextChoiceAnswerFormat = ORKAnswerFormat.choiceAnswerFormat(with: .singleChoice, textChoices: textChoices)
+    let textChoiceStep = ORKQuestionStep(identifier: "TextChoiceQuestionStep", title: textChoiceStepTitle, answer: textChoiceAnswerFormat)
+    steps += [textChoiceStep]
+    
+    //Image Choice Question
+    let imageChoiceQuestionStepTitle = "Börk Børk Björk?"
+    let imageChoiceTuples = [
+        (UIImage(named: "Germany")!, "Germany"),
+        (UIImage(named: "Russia")!, "Russia"),
+        (UIImage(named: "France")!, "France"),
+        (UIImage(named: "Sweden")!, "Sweden"),
+    ]
+    let imageChoices : [ORKImageChoice] = imageChoiceTuples.map {
+        return ORKImageChoice(normalImage: $0.0, selectedImage: nil, text: $0.1, value: $0.1 as NSCoding & NSCopying & NSObjectProtocol)
+    }
+    let imageChoiceAnswerFormat: ORKImageChoiceAnswerFormat = ORKAnswerFormat.choiceAnswerFormat(with: imageChoices)
+    let imageChoiceQuestionStep = ORKQuestionStep(identifier: "ImageChoiceQuestionStep", title: imageChoiceQuestionStepTitle, answer: imageChoiceAnswerFormat)
+    steps += [imageChoiceQuestionStep]
+    
+    //Other Tasks
+    //Date
+    //Boolean
+    //Continuous
+    
+    //Summary
+    let summaryStep = ORKCompletionStep(identifier: "SummaryStep")
+    summaryStep.title = "Thank you."
+    summaryStep.text = "We appreciate your time."
+    steps += [summaryStep]
+    
+    return ORKOrderedTask(identifier: "SurveyTask", steps: steps)
+}
--- AppleWatchHeartRateCollection/Tasks/TasksViewController.swift
+++ AppleWatchHeartRateCollection/Tasks/TasksViewController.swift
@@ -2,7 +2,39 @@
 //  TasksViewController.swift
 //  AppleWatchHeartRateCollection
 //
-//  Created by 叶思帆 on 28/06/2018.
+//  Code from ORKSample/Activities/ActivityViewController
+//  Original Copyright Document:
+/*
+ Copyright (c) 2015, Apple Inc. All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+ 
+ 1.  Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 
+ 2.  Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation and/or
+ other materials provided with the distribution.
+ 
+ 3.  Neither the name of the copyright holder(s) nor the names of any contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission. No license is granted to the trademarks of
+ the copyright holders even if such marks are included in this software.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+//
+//  Modified by 叶思帆 on 28/06/2018.
 //  Copyright © 2018 Sifan Ye. All rights reserved.
 //
 
@@ -10,8 +42,80 @@ import UIKit
 
 import ResearchKit
 
+enum Task: Int {
+    case survey, heartrate
+    
+    static var allValues: [Task] {
+        var index = 0
+        return Array (
+            AnyIterator {
+                let returnedElement = self.init(rawValue: index)
+                index = index + 1
+                return returnedElement
+            }
+        )
+    }
+    
+    var title: String {
+        switch self {
+            case .survey:
+                return "Survey"
+            case .heartrate:
+                return "Heart Rate"
+        }
+    }
+    
+    var subtitle: String {
+        switch self {
+            case .survey:
+                return "Answer # short questions"
+            case .heartrate:
+                return "Heart rate data collection"
+        }
+    }
+}
+
 class TasksViewController: UITableViewController {
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard section == 0 else { return 0 }
+        
+        return Task.allValues.count
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "activityCell", for: indexPath)
+        
+        if let activity = Task(rawValue: (indexPath as NSIndexPath).row) {
+            cell.textLabel?.text = activity.title
+            cell.detailTextLabel?.text = activity.subtitle
+        }
+        
+        return cell
+    }
     
+    // MARK: UITableViewDelegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let activity = Task(rawValue: (indexPath as NSIndexPath).row) else { return }
+        
+        let taskViewController: ORKTaskViewController
+        switch activity {
+            case .survey:
+                taskViewController = ORKTaskViewController(task: SurveyTask, taskRun: NSUUID() as UUID)
+            case .heartrate:
+                taskViewController = ORKTaskViewController(task: SurveyTask, taskRun: NSUUID() as UUID)
+        }
+        
+        taskViewController.delegate = self
+        navigationController?.present(taskViewController, animated: true, completion: nil)
+    }
+}
+
+extension TasksViewController : ORKTaskViewControllerDelegate {
+    
+    func taskViewController(_ taskViewController: ORKTaskViewController, didFinishWith reason: ORKTaskViewControllerFinishReason, error: Error?) {
+        // Handle results using taskViewController.result
+        taskViewController.dismiss(animated: true, completion: nil)
+    }
 }
